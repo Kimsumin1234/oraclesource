4adba241743139058d142e9e862b5f67b253aa1e
sumin <whshaks14@gmail.com>
2024-01-30 18:00:52 +0900

    제한조건권한부여

--- system.sql
+++ system.sql
@@ -1,5 +1,6 @@
 -- scott 권한 부여
 GRANT CONNECT, resource TO scott;
+GRANT CREATE VIEW TO scott;
 
 
 -- 데이터를 저장할 수 있는 저장공간 할당
@@ -8,3 +9,74 @@ ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
 ALTER USER SCOTT QUOTA 10M ON USERS;
 
 alter session set "_oracle_script"=true;
+
+
+
+-- DBA 권한을 가진 사용자만 사용자를 생성하고 권한 부여
+-- SYSTEM, sys as sysdba
+
+-- CREATE USER 아이디 IDENTIFIED BY 비밀번호;
+
+-- TEST 생성
+-- 공통 사용자 또는 롤 이름이 부적합합니다.(c##)
+-- 21c XE 버전에서의 요구사항(다른버전은 다를수도 있음)
+CREATE USER c##test IDENTIFIED BY test;
+
+-- CREATE SESSION 권한을 가지고있지 않음 로그온이 거절
+-- 권한 부여
+-- SESSION, CREATE(USER, TABLE, SEQUENCE, VIEW...)
+-- 권한 그룹 -> 롤(관련있는 권한들을 묶어놈)
+-- CONNECT, RESOURCE
+-- CONNECT(SESSION, CREATE TABLE, CREATE VIEW...)
+-- RESOURCE(SEQUENCE, 프로시저, 테이블...)
+
+-- GRANT 권한이름,.... TO 아이디
+-- GRANT 롤이름,.... TO 아이디
+GRANT CONNECT, RESOURCE TO c##test;
+
+-- USER 생성 구문
+-- 아이디,비번,권한,용량...
+CREATE USER c##test2 IDENTIFIED BY test
+DEFAULT TABLESPACE USERS
+TEMPORARY TABLESPACE TEMP
+QUOTA 10M ON USERS;
+
+GRANT CONNECT, RESOURCE TO c##test2;
+
+-- REVOKE 취소할 권한이름,... FROM 아이디
+
+-- 비밀번호 변경
+ALTER USER c##test2 IDENTIFIED BY test;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
